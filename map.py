e2125975dc2a46d6cb31dcba9054bf2081f49444
Florian <florian@flova.de>
2018-10-16 22:55:09 +0200

    Puts the Snap visualizer in an own class.

--- map.py
+++ map.py
@@ -3,45 +3,60 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 from google_map import GoogleLoc
+from progressbar import ProgressBar
 
-print("Init finished")
+class SnapLoc():
+    def __init__(self):
+        self.pbar = ProgressBar()
+        self.map = None
+        self.locations = self.importer()
 
-with open('Snap/json/location_history.json') as f:
-    data = json.load(f)["Locations You Have Visited"]
-    
-print("Data loaded")
+    def importer(self):
+        with open('Snap/json/location_history.json') as f:
+            data = json.load(f)["Locations You Have Visited"]
+        print("File loaded")
+        print(str(len(data)) + " Standortdaten.")
+        return data
 
-fig = plt.figure(figsize=(8, 8))
-m = Basemap(projection='lcc',
-            resolution='i',
-            width=6E6, height=6E6, 
-            lat_0=53, lon_0=9,)
-m.bluemarble()
+    def init_map(self):
+        fig = plt.figure(figsize=(8, 8))
+        m = Basemap(projection='lcc',
+                    resolution=None,
+                    width=6E6, height=6E6, 
+                    lat_0=53, lon_0=9,)
+        # m.bluemarble()
+        self.map = m
+        print("Map loaded")
 
-print("Map gernerated")
+    def converter(self, data):
+        latA = list()
+        lonA = list()
+        for dataset in self.pbar(data):
+            Lat = float(dataset['Latitude, Longitude'].split(",")[0][0:6])
+            Lon = float(dataset['Latitude, Longitude'].split(",")[1][0:6])
+            latA.append(Lat)
+            lonA.append(Lon)
+        print("Data parsed and loaded in memory")
+        return latA, lonA
 
-latA = list()
-lonA = list()
+    def plot(self, latA, lonA):
+        x, y = self.map(lonA, latA)
+        self.map.plot(x, y, 'o-', markersize=5, linewidth=1) 
+        print("Lines drawed")
 
-for dataset in data:
-    Lat = float(dataset['Latitude, Longitude'].split(",")[0][0:6])
-    Lon = float(dataset['Latitude, Longitude'].split(",")[1][0:6])
-    
-    latA.append(Lat)
-    lonA.append(Lon)
+    def run(self):
+        self.plot(*self.converter(self.locations))
 
-print("Data parsed and loaded in memory")
 
-print("Load Google")
-worker = GoogleLoc()
-worker.set_map(m)
-worker.run()
-print("Finished Google Data")
-
-x, y = m(lonA, latA)
-m.plot(x, y, 'o-', markersize=5, linewidth=1) 
-
-print("Lines drawed")
-
-plt.title('Snap Locations')
-plt.show()
+if __name__ == "__main__":
+    print("Load Snap")
+    snap_worker = SnapLoc()
+    snap_worker.init_map()
+    snap_worker.run()
+    print("Load Google")
+    g_worker = GoogleLoc()
+    g_worker.set_map(snap_worker.map)
+    g_worker.run()
+    print("Finished Google Data")
+    plt.title('Snap Locations')
+    plt.show()
