16920b37b47756e3c9e2b7b67c4eed352512d32b
Florian <florian@flova.de>
2018-11-07 10:39:21 +0100

    Adds emoji analyses

---
+++ Whatsapp/emoji_analyse.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import time
+import emoji
+import re
+import operator
+from datetime import datetime
+from collections import Counter
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+
+# Search in single chat
+def analyse_chat(messages):
+    emoji_list = list()
+    for message in messages:
+        for character in message["message"]:
+             if character in emoji.UNICODE_EMOJI:
+                 emoji_list.append(character)
+    return emoji_list
+
+me = "Florian Vahl"
+
+blacklist = ["Bosshood👍👌",]
+
+with open("output.json") as f:
+    chats = json.load(f)
+
+# Search in all chats
+result = list()
+for chat in chats:
+    if chat not in blacklist:
+        result.extend(analyse_chat(chats[chat]))
+
+histogram = Counter(result)
+reduced_histogram = histogram.most_common(20)
+
+reduced_histogram = dict(reduced_histogram)
+
+plt.figure(figsize=(10,9))
+plt.title("Emoji 😂👍 counter ", fontsize=20)
+plt.margins(0.01)
+plt.bar(reduced_histogram.keys(), reduced_histogram.values())
+plt.show()
