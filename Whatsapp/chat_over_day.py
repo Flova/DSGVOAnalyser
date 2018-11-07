241410ed2fc3382a19758a8f39225fb049b1a1a7
Florian <florian@flova.de>
2018-11-07 16:39:41 +0100

    Adds script to display avg chat times.

---
+++ Whatsapp/chat_over_day.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import time
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+me = "Florian Vahl"
+only_me = True
+
+with open("output.json") as f:
+    chats = json.load(f)
+
+day = [0]*24
+for chat in chats:
+    for message in chats[chat]:
+        if not(only_me) or message["author"] == me:
+            day[int(datetime.strptime(message["time"], '%Y-%m-%dT%H:%M:%S').strftime('%H'))] += 1
+
+lables = []
+for hour, value in enumerate(day):
+    lables.append(datetime.strptime(str(hour), '%H').strftime('%H:%M'))
+
+plt.figure(figsize=(10,6))
+plt.title("Chat over time", fontsize=20)
+plt.xlabel('Hour', fontsize=15)
+plt.ylabel('n', fontsize=15)
+plt.xticks(range(len(lables)), lables, rotation='vertical')
+plt.margins(0.01)
+plt.tight_layout()
+plt.plot(day, 'k')
+plt.show()
+
