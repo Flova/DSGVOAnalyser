79aa7bfee9c4db2cc439b1224a55d4d939467cd4
Florian <florian@flova.de>
2019-03-26 01:25:12 +0100

    refactors first strike

--- Whatsapp/first_strike.py
+++ Whatsapp/first_strike.py
@@ -7,47 +7,57 @@ from datetime import datetime
 from statistics import mean
 import matplotlib.pyplot as plt
 
-def to_timestamp(date_string):
-    return time.mktime(datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S').timetuple())
 
-me = "Florian Vahl"
-chat_partner = ""
-threshold = 24 #Stunden
-blacklist = ["!Bosshood👍👌",]
+class WhatsappFirstStrike():
+    def __init__(self, file="output.json", me="Florian Vahl", partner="", blacklist=["!Bosshood👍👌",], threshold=24):
+        self.me = me
+        self.partner = partner
+        self.threshold = threshold
+        self.blacklist = blacklist
 
-with open("output.json") as f:
-    chats = json.load(f)
+        with open(file) as f:
+            self.chats = json.load(f)
 
-average_values = []
+        self.average_values = []
 
-def calc(chat_partner):
-    chat = chats[chat_partner]
-    diff = [1,1]
+    def to_timestamp(self, date_string):
+        return time.mktime(datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S').timetuple())
 
-    for index, message in enumerate(chat):
-        last = index - 1
-        if last > 0:
-            temp = to_timestamp(message["time"]) - to_timestamp(chat[last]["time"])
-            if temp > (threshold * 3600):
-                if message["author"] == me:
-                    diff[0] += 1
-                else:
-                    diff[1] += 1
-    # print("{} hat {} mal ein Gespräch angfangen | {} hat {} mal ein Gespräch angfangen".format(me, diff[0], chat_partner, diff[1]))
-    return round(diff[0] - diff[1]) / (abs(diff[0]) + abs(diff[1]))
+    def calc(self, partner):
+        chat = self.chats[partner]
+        diff = [1,1]
 
-data = {}
-if chat_partner == "":
-    for chat in chats:
-        if chat not in blacklist:
-            data[chat] = calc(chat)
-else:
-    data[chat_partner] = calc(chat_partner)
+        for index, message in enumerate(chat):
+            last = index - 1
+            if last > 0:
+                temp = self.to_timestamp(message["time"]) - self.to_timestamp(chat[last]["time"])
+                if temp > (self.threshold * 3600):
+                    if message["author"] == self.me:
+                        diff[0] += 1
+                    else:
+                        diff[1] += 1
+        # print("{} hat {} mal ein Gespräch angfangen | {} hat {} mal ein Gespräch angfangen".format(me, diff[0], partner, diff[1]))
+        return round(diff[0] - diff[1]) / (abs(diff[0]) + abs(diff[1]))
 
-plt.figure(figsize=(10,8))
-plt.title("Chat start diff", fontsize=20)
-plt.margins(0.01)
-plt.bar(data.keys(), data.values())
-plt.xticks(rotation=90)
-plt.tight_layout()
-plt.show()
+
+    def plot(self):
+        data = {}
+        if self.partner == "":
+            for chat in self.chats:
+                if chat not in self.blacklist:
+                    data[chat] = self.calc(chat)
+        else:
+            data[self.partner] = calc(self.partner)
+
+        plt.figure(figsize=(10,8))
+        plt.title("Chat start diff", fontsize=20)
+        plt.margins(0.01)
+        plt.bar(data.keys(), data.values())
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    tool = WhatsappFirstStrike()
+    tool.plot()
